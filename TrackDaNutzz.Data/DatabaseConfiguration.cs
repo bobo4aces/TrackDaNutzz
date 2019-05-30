6fea020f4794dbf4f965a62af13a55b492e5c593
bobo4aces <yo_therapper@abv.bg>
2019-05-30 17:56:57 +0300

    added DbContext and Database Configuration

---
+++ TrackDaNutzz.Data/DatabaseConfiguration.cs
@@ -0,0 +1,7 @@
+﻿namespace TrackDaNutzz.Data
+{
+    internal class DatabaseConfiguration
+    {
+        internal const string ConfigurationString = "Server=.,Database=TrackDaNutzzDB;Integrated Security=True;";
+    }
+}
---
+++ TrackDaNutzz.Data/TrackDaNutzzDbContext.cs
@@ -0,0 +1,28 @@
+﻿using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace TrackDaNutzz.Data
+{
+    public class TrackDaNutzzDbContext : DbContext
+    {
+        public TrackDaNutzzDbContext(DbContextOptions options) 
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlServer(DatabaseConfiguration.ConfigurationString);
+            }
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
