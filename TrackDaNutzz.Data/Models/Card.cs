6e66b93112b3c1345f7db70de62475a6955ad548
bobo4aces <yo_therapper@abv.bg>
2019-06-04 21:31:57 +0300

    deleted Card Entity

--- TrackDaNutzz.Data/Models/Card.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace TrackDaNutzz.Data.Models
-{
-    public class Card
-    {
-        public int Id { get; protected set; }
-        public string Suit { get; protected set; }
-        public string Rank { get; protected set; }
-    }
-}
--- TrackDaNutzz.Data/TrackDaNutzzDbContext.cs
+++ TrackDaNutzz.Data/TrackDaNutzzDbContext.cs
@@ -9,7 +9,6 @@
         //TODO: Add more DbSets. Check Protected Set
         public DbSet<TrackDaNutzz.Data.Models.Action> Action { get; protected set; }
         public DbSet<Board> Boards { get; protected set; }
-        public DbSet<Card> Cards { get; protected set; }
         public DbSet<Hand> Hands { get; protected set; }
         public DbSet<HandPlayer> HandPlayers { get; protected set; }
         public DbSet<HandStatistics> HandStatistics { get; protected set; }
@@ -39,7 +38,6 @@
         {
             modelBuilder.ApplyConfiguration(new ActionEntityTypeConfiguration());
             modelBuilder.ApplyConfiguration(new BoardEntityTypeConfiguration());
-            modelBuilder.ApplyConfiguration(new CardEntityTypeConfiguration());
             modelBuilder.ApplyConfiguration(new HandEntityTypeConfiguration());
             modelBuilder.ApplyConfiguration(new HandPlayerEntityTypeConfiguration());
             modelBuilder.ApplyConfiguration(new HandStatisticsEntityTypeConfiguration());
