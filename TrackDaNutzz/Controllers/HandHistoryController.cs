6723b9ed8091ae693697f566408d7e3d6240e83e
bobo4aces <yo_therapper@abv.bg>
2019-07-02 22:14:41 +0300

    added basic views for Hand History Import and Export

---
+++ TrackDaNutzz/Controllers/HandHistoryController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrackDaNutzz.Controllers
+{
+    public class HandHistoryController : Controller
+    {
+        [HttpGet]
+        public IActionResult Import()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Export()
+        {
+            return View();
+        }
+    }
+}
