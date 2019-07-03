958d2524535be86e6da2120f024b65f8c19c7b6a
bobo4aces <yo_therapper@abv.bg>
2019-07-03 22:32:21 +0300

    added basic views for players, stakes and hand details

---
+++ TrackDaNutzz/Controllers/HandsController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrackDaNutzz.Controllers
+{
+    public class HandsController : Controller
+    {
+        public IActionResult Details()
+        {
+            return View();
+        }
+    }
+}
