9deb56b5f3f94357e44adc71eacaf0d4c144d504
Boris Spasov <yo_therapper@abv.bg>
2019-08-06 23:46:44 +0300

    added Extensions Tests

--- TrackDaNutzz.Tests/TrackDaNutzz.Services.Tests/Extensions.cs
+++ TrackDaNutzz.Tests/TrackDaNutzz.Services.Tests/Extensions.cs
@@ -1,13 +1,58 @@
-﻿using Xunit;
+﻿using System;
+using TrackDaNutzz.Services.Common.Enums;
+using TrackDaNutzz.Services.Extensions;
+using Xunit;
 
 namespace TrackDaNutzz.Tests.TrackDaNutzz.Services.Tests
 {
     public class Extensions
     {
+        //DateTime Before(this DateTime date, TimePeriod timePeriod, int timePeriodCount)
         [Fact]
-        public void Test1()
+        public void TestBefore_WithOneDayBefore_ShouldReturnOneDateBefore()
         {
+            DateTime date = new DateTime(2019, 8, 3);
 
+            DateTime dayBeforeDate = date.Before(TimePeriod.Day,-1);
+
+            DateTime expected = new DateTime(2019, 8, 2);
+            DateTime actual = dayBeforeDate;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestBefore_WithOneMonthBefore_ShouldReturnOneMonthBefore()
+        {
+            DateTime date = new DateTime(2019, 8, 3);
+
+            DateTime dayBeforeDate = date.Before(TimePeriod.Month, -1);
+
+            DateTime expected = new DateTime(2019, 7, 3);
+            DateTime actual = dayBeforeDate;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestBefore_WithOneYearBefore_ShouldReturnOneYearBefore()
+        {
+            DateTime date = new DateTime(2019, 8, 3);
+
+            DateTime dayBeforeDate = date.Before(TimePeriod.Year, -1);
+
+            DateTime expected = new DateTime(2018, 8, 3);
+            DateTime actual = dayBeforeDate;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestBefore_WithOneYearBeforeAndInvalidYear_ShouldThrowArgumentOutOfRangeException()
+        {
+            DateTime date = new DateTime();
+
+            Assert.Throws(typeof(ArgumentOutOfRangeException), () => date.Before(TimePeriod.Year, -1));
         }
     }
 }
