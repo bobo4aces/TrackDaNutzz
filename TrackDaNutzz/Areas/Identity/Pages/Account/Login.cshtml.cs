b50417beefd21496fef21fcfd48f6a64af786a14
Boris Spasov <yo_therapper@abv.bg>
2019-08-06 20:06:03 +0300

    added validation for username at login input model

--- TrackDaNutzz/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ TrackDaNutzz/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -40,6 +40,7 @@ namespace TrackDaNutzz.Areas.Identity.Pages.Account
         {
             [Required]
             [Display(Name = "Username")]
+            [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 3)]
             public string Username { get; set; }
 
             [Required]
