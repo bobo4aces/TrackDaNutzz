b8ffc31f5e5ecbe8d52a35a408168fce3beaa08a
Boris Spasov <yo_therapper@abv.bg>
2019-08-08 21:54:04 +0300

    added VariantsServiceTests

---
+++ TrackDaNutzz.Tests/TrackDaNutzz.Services.Tests/StatisticsServiceTests.cs
@@ -0,0 +1,25 @@
+﻿using Xunit;
+
+namespace TrackDaNutzz.Tests.TrackDaNutzz.Services.Tests
+{
+    public class StatisticsServiceTests
+    {
+        //Dictionary<string, long> AddStatistics(ImportHandDto handDto);
+        //Statistic CreateStatistic(ImportHandDto handDto, string playerName, List<BettingActionDto> playerPreflopBettingActions, List<BettingActionDto> playerFlopBettingActions, List<BettingActionDto> playerAllBettingActions, SeatInfoDto seatInfoDto);
+        //bool GetContinuationBet(List<BettingActionDto> playerPreflopBettingActions, List<BettingActionDto> playerFlopBettingActions);
+        //decimal GetBigBlindsWon(decimal moneyWon, decimal bigBlind);
+        //decimal GetMoneyWon(ImportHandDto handDto, SeatInfoDto seatInfoDto);
+        //decimal GetAggressionFactor(int totalRaises, int totalBets, int totalCalls);
+        //bool GetFourBet(List<BettingActionDto> playerPreflopBettingActions, string playerName);
+        //bool GetThreeBet(List<BettingActionDto> playerPreflopBettingActions, string playerName);
+        //bool GetPreFlopRaise(List<BettingActionDto> playerPreflopBettingActions);
+        //bool GetVoluntaryPutMoneyInPot(List<BettingActionDto> playerPreflopBettingActions);
+        //IQueryable<StatisticsDto> GetStatisticsById(params long[] statisticsIds);
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ TrackDaNutzz.Tests/TrackDaNutzz.Services.Tests/VariantsServiceTests.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using TrackDaNutzz.Data;
+using TrackDaNutzz.Services.Dtos.Hands;
+using TrackDaNutzz.Services.Dtos.Variants;
+using TrackDaNutzz.Services.Stakes;
+using TrackDaNutzz.Services.Variant;
+using Xunit;
+
+namespace TrackDaNutzz.Tests.TrackDaNutzz.Services.Tests
+{
+    public class VariantsServiceTests
+    {
+        //int AddVariant(HandInfoDto handInfoDto);
+        //VariantDto GetVariantById(int variantId);
+
+        [Fact]
+        public void TestAddVariant_WithOneHandInfoDto_ShouldReturnCorrectId()
+        {
+            DbContextOptions<TrackDaNutzzDbContext> options = new DbContextOptionsBuilder<TrackDaNutzzDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            TrackDaNutzzDbContext context = new TrackDaNutzzDbContext(options);
+
+            VariantsService variantService = new VariantsService(context);
+
+            HandInfoDto handInfoDto = this.GetTestHandInfoDto();
+            int variantId = variantService.AddVariant(handInfoDto);
+
+            int expected = 1;
+            int actual = variantId;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void TestGetVariantById_WithOneVariantId_ShouldReturnCorrectVariant()
+        {
+            DbContextOptions<TrackDaNutzzDbContext> options = new DbContextOptionsBuilder<TrackDaNutzzDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            TrackDaNutzzDbContext context = new TrackDaNutzzDbContext(options);
+
+            VariantsService variantService = new VariantsService(context);
+
+            HandInfoDto handInfoDto = this.GetTestHandInfoDto();
+            int variantId = variantService.AddVariant(handInfoDto);
+
+            VariantDto variantDto = variantService.GetVariantById(variantId);
+            int expected = 2;
+            int actual = variantDto.Id;
+
+            Assert.Equal(expected, actual);
+        }
+
+        private HandInfoDto GetTestHandInfoDto()
+        {
+            HandInfoDto handInfoDto = new HandInfoDto()
+            {
+                BigBlind = 0.02m,
+                ClientName = "PokerStars",
+                Currency = "USD",
+                CurrencySymbol = '$',
+                HandNumber = 202717426423,
+                Limit = "No Limit",
+                LocalTime = DateTime.Now,
+                LocalTimeZone = "ET",
+                SmallBlind = 0.01m,
+                Time = DateTime.Now,
+                TimeZone = "EET",
+                VariantName = "Hold'em",
+            };
+            return handInfoDto;
+        }
+    }
+}
