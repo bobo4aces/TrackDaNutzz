4511803505bee9a53c77ca044ff2c64b29aab933
Boris Spasov <yo_therapper@abv.bg>
2019-08-13 00:11:00 +0300

    .

--- TrackDaNutzz.Data/DatabaseConfiguration.cs
+++ TrackDaNutzz.Data/DatabaseConfiguration.cs
@@ -2,6 +2,6 @@
 {
     public class DatabaseConfiguration
     {
-        public const string ConnectionString = "Server=DESKTOP-FEQ2VCO\\SQLEXPRESS;Database=TrackDaNutzzDB;Integrated Security=True;Trusted_Connection=True;";
+        public const string ConnectionString = "Data Source=tcp:bobo4acessqlserver.database.windows.net,1433;Initial Catalog=TrackDaNutzzDb;User Id=bobo4aces@bobo4acessqlserver.database.windows.net;Password=AzurePass4;";
     }
 }
